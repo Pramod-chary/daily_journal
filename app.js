bc2f2933188ff316f4f06c49f9ac8f0a164b9552
Pramod-chary <pramodchary24@gmail.com>
2023-01-16 10:02:22 +0530

    render commit

--- app.js
+++ app.js
@@ -56,5 +56,5 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000");
+  console.log("Server started");
 });
